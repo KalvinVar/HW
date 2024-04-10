1500023b396908852ae606ab0311728694b43f32
Kalvin Varona <kalvinlancepvarona@gmail.com>
2024-04-10 15:44:10 -0700

    added math problems and variables

---
+++ JS/test.js
@@ -0,0 +1,15 @@
+
+/* 2 Examples of declaring variables in JS */
+let num1 = 10;
+
+let num2;
+
+let num3;
+
+/* 3 Examples of basic arithmetic in JS */
+
+
+
+
+/* 2 Examples of using variables in arithmetic operations in JS */
+let print = document.getElementById('output')
