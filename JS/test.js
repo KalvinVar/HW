30ad94d700d357e1954424e38919448722266ca3
Kalvin Varona <kalvinlancepvarona@gmail.com>
2024-04-27 11:45:46 -0700

    made and for loop to output the data in a array

--- JS/test.js
+++ JS/test.js
@@ -1,15 +1,55 @@
+// USE Alt + Shift + F to fix indent
+// 1) Explain the structure of the JSON
+/* JSON organizes weather data: cities, temperatures, chance of rain. The webpage would take 
+that JSON payload and display the weather forecast for different cities around the world.*/
+let weather_data = {
+    data: [
+        {
+            city: "New York",
+            temp: 18,
+            rain: 30
+        },
+        {
+            city: "Los Angeles",
+            temp: 25,
+            rain: 10
+        },
+        {
+            city: "London",
+            temp: 12,
+            rain: 50
+        },
+        {
+            city: "Tokyo",
+            temp: 20,
+            rain: 20
+        },
+        {
+            city: "Sydney",
+            temp: 22,
+            rain: 15
+        }
+    ]
+};
 
-/* 2 Examples of declaring variables in JS */
-let num1 = 10;
+const table = document.getElementById("columbody");
+// 2) Write the loop and explain while making it
+for (i in weather_data.data) {
+const row = document.createElement("tr");
 
-let num2;
+const cell1 = document.createElement("td");
+cell1.textContent = weather_data.data[i].city;
+row.appendChild(cell1);
 
-let num3;
+const cell2 = document.createElement("td");
+cell2.textContent = weather_data.data[i].temp;
+row.appendChild(cell2);
 
-/* 3 Examples of basic arithmetic in JS */
+const cell3 = document.createElement("td");
+cell3.textContent = weather_data.data[i].rain;
+row.appendChild(cell3);
 
+table.appendChild(row);
+};
 
-
-
-/* 2 Examples of using variables in arithmetic operations in JS */
-let print = document.getElementById('output')
+// 3) Save it
